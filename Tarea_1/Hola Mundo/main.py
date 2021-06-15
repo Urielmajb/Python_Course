d5e8a722d5ef2582a42fee6f875a15745098581c
urielmajb <urielmajb@gmail.com>
2021-06-15 15:46:01 -0600

    Tarea 2

--- Tarea_1/Hola Mundo/main.py
+++ Tarea_1/Hola Mundo/main.py
@@ -35,9 +35,18 @@ print(Condicion)
 
 #Funcion input, procesa la entrada del usuario.
 
-num1 = int(input("Ingrese el primer numero"))
-num2 = int(input("Ingrese el segundo numero"))
-suma = num1 + num2
-resta = num1 - num2
-division = num1 / num2
-print("Los resultados son: ", suma , " " , resta , " " , division)
+#num1 = int(input("Ingrese el primer numero"))
+#num2 = int(input("Ingrese el segundo numero"))
+#suma = num1 + num2
+#resta = num1 - num2
+#division = num1 / num2
+#print("Los resultados son: ", suma , " " , resta , " " , division)
+
+#Ejercicio Califiacion del dia
+
+#dia = input("¿Como estuvo el día (1 a 10)?")
+#print("Mi dia estuvo: ", dia)
+
+titulo = input("Proporciona el título: ")
+autor = input("Proporciona el autor: ")
+print(titulo, "fue escrito por " ,autor )
