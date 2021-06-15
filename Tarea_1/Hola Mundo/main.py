b14e6f8f94556fa3b9a9a33a05ea4db0d167c116
urielmajb <urielmajb@gmail.com>
2021-06-15 15:27:15 -0600

    Condicion bool

--- Tarea_1/Hola Mundo/main.py
+++ Tarea_1/Hola Mundo/main.py
@@ -32,3 +32,12 @@ print("Mi cantante favorita es: " + miArtistafav)
 
 Condicion = 1 > 3
 print(Condicion)
+
+#Funcion input, procesa la entrada del usuario.
+
+num1 = int(input("Ingrese el primer numero"))
+num2 = int(input("Ingrese el segundo numero"))
+suma = num1 + num2
+resta = num1 - num2
+division = num1 / num2
+print("Los resultados son: ", suma , " " , resta , " " , division)
