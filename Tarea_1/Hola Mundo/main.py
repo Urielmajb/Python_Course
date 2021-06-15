6b0c5c2f660becd0e7177c2f5f33386b66755244
urielmajb <urielmajb@gmail.com>
2021-06-15 11:00:11 -0600

    actualizacion curso

---
+++ Tarea_1/Hola Mundo/main.py
@@ -0,0 +1,28 @@
+# print ("Hola mundo con python woooo!")
+
+miVariable = "Hola mundo con python woooo!"
+print(miVariable)
+
+x = 10
+y = 2
+suma = x + y
+
+print (suma, x, y)
+
+print (id(x))
+print (id(y))
+print (id(suma))
+print(type(x))
+
+x = 15
+print(x)
+print(type(x))
+
+y=3.14
+print(y)
+print(type(y))
+
+nombre = 'Urielma Jiron'
+print(nombre)
+print (type(nombre))
+
