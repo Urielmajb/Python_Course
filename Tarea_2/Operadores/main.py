81786bebec6f5d5e889b34d76bcb966d1101e4b9
urielmajb <urielmajb@gmail.com>
2021-06-15 18:27:36 -0600

    tarea2

--- Tarea_2/Operadores/main.py
+++ Tarea_2/Operadores/main.py
@@ -1,8 +1,16 @@
 #Operadores
 
-alto = int(input("Proporcionar el alto: "))
-ancho = int(input("Proporcionar el ancho: "))
-area = alto * ancho
-perimetro = ((alto + ancho) * 2)
+#alto = int(input("Proporcionar el alto: "))
+#ancho = int(input("Proporcionar el ancho: "))
+#area = alto * ancho
+#perimetro = ((alto + ancho) * 2)
+#print(f'El area es: {area}' + f'y el perimetro: {perimetro}')
 
-print(f'El area es: {area}' + f'y el perimetro: {perimetro}')
+miVariable = 11
+print(miVariable)
+
+miVariable -= 2
+print(miVariable)
+
+miVariable += 3
+print(miVariable)
